61a37ec9339d02a9751e71b600893bd676dace08
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-04-18 01:57:20 +0800

    :art: Add style

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,22 +1,13 @@
 <script setup>
-import { computed } from 'vue';
-import { useStore } from 'vuex';
-import router from "../router";
-
-const store = useStore()
-const userState = computed(() => store.state.auth.userDetails)
-
-const logOut = () => {
-  store.dispatch('logOutAction')
-  router.push('/login')
-}
+import ProfileCard from '../components/ProfileCard.vue'
 
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <p v-text="userState.userData.email" />
-    <button class="bg-[#2B2E4A] rounded-full drop-shadow-lg text-white text-md h-9 w-[85px] hover:bg-[#FF9000] transition ease-in-out" @click="logOut">LogOut</button>
+  <div class="flex flex-col items-center">
+    <div class="container mx-auto">
+      <ProfileCard />
+    </div>
   </div>
 </template>
 
